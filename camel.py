e8802c7794a465572fad3bcef5b496fd0f6f25d4
Vivo2005 <lvm1@kent.ac.uk>
2026-02-02 14:17:35 +0000

    struggling

--- camel.py
+++ camel.py
@@ -1,1 +1,14 @@
 # Enter your code here
+camel_variable=input(("name your variable:"))
+
+def snake(a):
+    c=(a.replace('_',' '))
+    #keep the terms seperate to captalize second terms
+    d=c.title() 
+    #learned how to slice in order to pick capitalized letterw
+    e=(d[:1].lower()+d[1:])
+    return e.replace(' ','')
+    
+#tested for 2+ terms
+print(snake(camel_variable))
+#just realized i did it wrong but i spent too long to delete it
--- coffee.py
+++ coffee.py
@@ -1,1 +1,21 @@
 #Your code goes here
+debt=int(75)
+payment=int(input("insert coins:"))
+remaining=int(debt-payment)
+
+while 
+
+def machine():
+    if payment>=debt:
+        print("change dispensed",(payment-debt)) 
+    else:
+        print("remaining balance",(remaining))
+        p2=("insert coins:")
+        if p2>= remaining:
+
+
+machine()
+
+
+
+   
